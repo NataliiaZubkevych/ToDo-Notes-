656bb814b0b73fdb9fbb4e354912699256670d36
Nataliia <55789002+NataliiaZubkevych@users.noreply.github.com>
2021-02-21 01:54:00 +0200

    git add
    add.project

---
+++ ToDo_/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ToDo_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Tables = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Tables
+            // 
+            this.Tables.FormattingEnabled = true;
+            this.Tables.Location = new System.Drawing.Point(42, 57);
+            this.Tables.Name = "Tables";
+            this.Tables.Size = new System.Drawing.Size(257, 21);
+            this.Tables.TabIndex = 9;
+            this.Tables.SelectedIndexChanged += new System.EventHandler(this.Tables_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(682, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(682, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 21);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Execute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.DataSet);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(634, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Get ToDo";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(42, 88);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(717, 336);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 450);
+            this.Controls.Add(this.Tables);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox Tables;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ ToDo_/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace ToDo_
+{
+    public partial class Form1 : Form
+    {
+        private SqlDataReader reader;
+        SqlDataAdapter da = null;
+        private DataTable table;
+        private SqlConnection conn;
+        SqlCommandBuilder cmd = null;
+        DataSet set = null;
+        List<Category> categories = new List <Category>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection();
+            conn.ConnectionString = ConfigurationManager.ConnectionStrings["MyNotes"].ConnectionString;
+            Tables.Items.Add(new MyTable() { ShortName = "ToDo", SelectQuery = "Select * from ToDo" });
+            Tables.Items.Add(new MyTable() { ShortName = "Category", SelectQuery = "Select * from Category" });
+            Tables.SelectedIndex = 0;
+
+        }
+
+        class Category
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+        }
+        class MyTable
+        {
+            public string ShortName { get; set; }
+            public string SelectQuery { get; set; }
+            public override string ToString()
+            {
+                return ShortName;
+            }
+        }
+        public void GetCategories()
+        {
+            categories.Clear();
+            SqlDataReader rdr = null;
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("Category", conn);
+                rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Category category = new Category();
+                    category.ID = (int)rdr[0];
+                    category.Name = (string)rdr[1];
+                    categories.Add(category);
+                }
+            }
+            finally
+            {
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        private void show_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                GetCategories();
+                SqlCommand comm = new SqlCommand();
+                comm.CommandText = textBox1.Text;
+                comm.Connection = conn;
+                conn.Open();
+
+                dataGridView1.DataSource = null;
+
+                table = new DataTable();
+                reader = comm.ExecuteReader();
+                int line = 0;
+
+                while (reader.Read())
+                {
+                    if (line == 0)
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            table.Columns.Add(reader.GetName(i));
+                        }
+                        line++;
+                    }
+                    DataRow row = table.NewRow();
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        if (i == 6)
+                        {
+                            foreach (var item in categories)
+                            {
+                                if ((int)reader[i] == item.ID)
+                                {
+                                    row[i] = item.Name;
+                                }
+                            }
+                        }
+                        else row[i] = reader[i];
+                    }
+                    table.Rows.Add(row);
+                }
+                dataGridView1.DataSource = table;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+            }
+        }
+
+        private void DataSet(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView1.DataSource = null;
+
+                set = new DataSet();
+                string sql = textBox1.Text;
+                da = new SqlDataAdapter(sql, conn);
+                cmd = new SqlCommandBuilder(da);
+
+                da.Fill(set, "ToDo");
+                dataGridView1.DataSource = set.Tables["ToDo"];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            da.Update(set, "ToDo");
+        }
+
+        private void Tables_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = ((MyTable)Tables.SelectedItem).SelectQuery;
+        }
+    }
+}
